a516e7716d924bd9febdc24444888e08f844e7b7
Aditya Kumar <2005aditya.k@gmail.com>
2025-02-14 09:40:58 +0530

    Update

--- public/js/script.js
+++ public/js/script.js
@@ -2,15 +2,15 @@ const mobileMenuBtn = document.getElementById("mobile-menu-btn");
 const closeBtn = document.getElementById("close-btn");
 const mobileMenu = document.getElementById("mobile-menu");
 
-mobileMenuBtn.addEventListener("click", () => {
+mobileMenuBtn.addEventListener("click",()=>{
     mobileMenu.classList.toggle("hidden");
 });
-document.addEventListener("click", (event) => {
-    if (!mobileMenu.contains(event.target) && !mobileMenuBtn.contains(event.target)) {
+document.addEventListener("click", (event)=>{
+    if(!mobileMenu.contains(event.target) && !mobileMenuBtn.contains(event.target)){
         mobileMenu.classList.add("hidden");
     }
 });
 
-closeBtn.addEventListener("click", () => {
+closeBtn.addEventListener("click",()=>{
     mobileMenu.classList.add("hidden");
 });
