05b938f1fa4be2d7f1d018b8061acf763b3988e8
Aditya Kumar <2005aditya.k@gmail.com>
2025-03-23 11:40:26 +0530

    COde improvements and Isuue fix

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,11 @@
-const mobileMenuBtn = document.getElementById("mobile-menu-btn");
+const menuBtn = document.getElementById("menu-btn");
 const closeBtn = document.getElementById("close-btn");
 const mobileMenu = document.getElementById("mobile-menu");
 
-mobileMenuBtn.addEventListener("click",()=>{
-    mobileMenu.classList.toggle("hidden");
-});
-document.addEventListener("click", (event)=>{
-    if(!mobileMenu.contains(event.target) && !mobileMenuBtn.contains(event.target)){
-        mobileMenu.classList.add("hidden");
-    }
+menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.remove("-translate-y-full");
 });
 
-closeBtn.addEventListener("click",()=>{
-    mobileMenu.classList.add("hidden");
+closeBtn.addEventListener("click", () => {
+    mobileMenu.classList.add("-translate-y-full");
 });
---
+++ public/js/verifyotp.js
@@ -0,0 +1,80 @@
+window.onload = function() {
+    document.getElementById("otpinp1").focus();
+}
+
+const form = document.getElementById("otp-form");
+
+
+// Event listener for filling
+form.addEventListener("input", function(event) {
+    const otpinputs = [...form.getElementsByClassName("otp-input")];
+    const currentInput = event.target;
+    const currentIndex = otpinputs.indexOf(currentInput);
+
+    if(currentIndex !== -1 && currentInput.value.length > 0) {
+        const nextInput = otpinputs[currentIndex + 1];
+        if(nextInput) {
+            nextInput.focus();
+        }
+    }
+}) 
+
+// Event listener for deleting
+form.addEventListener("keydown", function(event) {
+    if(event.key == "Backspace") {
+        const otpinputs = [...form.getElementsByClassName("otp-input")];
+        const currentInput = event.target;
+        const currentIndex = otpinputs.indexOf(currentInput);
+
+        if(currentIndex > 0 && currentInput.value == "") {
+            otpinputs[currentIndex-1].focus();
+        }
+    }
+})
+
+let verifybtn = document.getElementById("verifybtn");
+verifybtn.addEventListener("click", async () => {
+    
+    const otpinputs = [...form.getElementsByClassName("otp-input")];
+
+    for (let i = 0; i < otpinputs.length; i++) {
+        const element = otpinputs[i];
+        if (element.value === "") {
+            alert("Incomplete OTP");
+            return;
+        }
+    }
+
+    const formdata = new FormData();
+
+    for (let i = 0; i < otpinputs.length; ++i) {
+        const element = otpinputs[i];
+
+        formdata.append(i , element.value);
+    }
+
+    let response;
+    try {
+        response = await fetch("http://localhost/ca2-project/backend/verify_otp.php", {
+            method: "POST",
+            credentials: "include",
+            body: formdata
+        })
+
+        const result = await response.json();
+
+        if(result.success) {
+            // redirect to dashboard
+            setTimeout(() => {
+                window.location.href = "dashboard.php";
+            }, 3000);
+        }
+        else {
+            alert(result.message);
+        }
+    }
+    catch(exception) {
+        console.log(exception);
+    }
+    
+})
